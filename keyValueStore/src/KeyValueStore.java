93727310d46325a1d64d4b9e5d845a301966c633
Surikm09 <skoneti1@binghamton.edu>
2017-11-22 21:29:50 -0500

    class to store key value pair

---
+++ keyValueStore/src/KeyValueStore.java
@@ -0,0 +1,16 @@
+
+public class KeyValueStore {
+	
+	public int key = 0;
+	public String value = null;
+	public long timestamp = 0;
+	
+	public KeyValueStore(int keyIn, String valueIn, long timeIn) {
+		
+		key = keyIn;
+		value = valueIn;
+		timestamp = timeIn;
+		
+	}
+
+}
