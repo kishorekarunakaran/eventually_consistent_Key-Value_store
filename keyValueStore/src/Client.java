4fecf03bbe8384c091c72017ab0fd1c52e5f0e0d
Surikm09 <skoneti1@binghamton.edu>
2017-11-22 21:31:15 -0500

    incoming connection check added

--- keyValueStore/src/Client.java
+++ keyValueStore/src/Client.java
@@ -1,19 +1,72 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+
 public class Client{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		
+	public static void main(String [] args) {
+					
+		ClientContext cc = new ClientContext(args[0]);
+		FileProcessor fp = new FileProcessor(args[1]);
+	    
+		cc.readFile(fp);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			String value = scan.next();
+			if(value == null){
+				System.out.println("Enter correct input");
+				break;	
+			}
+			System.out.println(value);
+			String[] splitValue;
+			splitValue = value.split(",");
+			KeyValue.KeyValueMessage.Builder keymessage = KeyValue.KeyValueMessage.newBuilder();
+			if(splitValue[0].equalsIgnoreCase("get")) {
+				int key = Integer.parseInt(splitValue[1]);
+				int clevel = Integer.parseInt(splitValue[2]);
+				KeyValue.Get.Builder getMethod = KeyValue.Get.newBuilder();
+				getMethod.setKey(key);
+				getMethod.setConsistency(clevel);
+				keymessage.setGetKey(getMethod.build());
+			}
+			if(splitValue[0].equalsIgnoreCase("put")) {
+				System.out.println("here");
+				int key = Integer.parseInt(splitValue[1]);
+				String input = splitValue[2];
+				int clevel = Integer.parseInt(splitValue[3]);
+				KeyValue.Put.Builder putMethod = KeyValue.Put.newBuilder();
+				putMethod.setKey(key);
+				putMethod.setValue(input);
+				putMethod.setConsistency(clevel);
+				keymessage.setPutKey(putMethod.build());
+			}
+			keymessage.setConnection(1);
+			try {
+				Socket send;
+				if(cc.sock == null) {
+					System.out.println("Socket");
+					send = new Socket(cc.coIp,cc.coPort);
+					cc.setSocket(send);
+				}
+				else {
+					send = cc.getSocket();
+				}
+				OutputStream out = send.getOutputStream();
+				keymessage.build().writeDelimitedTo(out);
+				out.flush();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+		
+	}
 }
--- keyValueStore/src/ReplicaServer.java
+++ keyValueStore/src/ReplicaServer.java
@@ -1,15 +1,43 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+
 public class ReplicaServer{
+	
+	public static void main(String[] args){
 
-
-
-
-
-
-
-
-
-
-
-
-
+		if(args.length != 2){
+			System.out.println("Usage: ./server server1 9090 conf.txt\n");
+			System.exit(0);
+		}
+		
+		ServerContext sc = new ServerContext(args[0],Integer.parseInt(args[1]));
+		FileProcessor fp = new FileProcessor(args[2]);
+		sc.readFile(fp);
+		
+		try {
+			sc.server = new ServerSocket(sc.port);
+		}
+		catch(IOException i) {
+			System.out.println(i);
+		}
+		Thread cl = new Thread();
+		
+		Socket request = null;
+		try {
+			request = sc.server.accept();
+			InputStream in = request.getInputStream();
+			KeyValue.KeyValueMessage incoming = KeyValue.KeyValueMessage.parseDelimitedFrom(in);
+			if(incoming.getConnection() == 1) {
+				sc.handleClient(incoming);
+			}
+			cl = new Thread(new Coordinator(request, sc));
+			cl.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
 }
