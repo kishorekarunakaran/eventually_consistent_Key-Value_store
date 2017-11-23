f572a55546d16e6bfff7a3d1da94a38a2c25e227
Surikm09 <skoneti1@binghamton.edu>
2017-11-22 21:27:45 -0500

    updated path

--- keyValueStore/client.sh
+++ keyValueStore/client.sh
@@ -1,5 +1,4 @@
 #!/bin/bash +vx
-LIB_PATH=$"/home/suri/Desktop/Github/eventually_consistent_Key-Value_store/lib
-/protobuf-java-3.4.0.jar"
+LIB_PATH=$"/home/suri/Desktop/Github/eventually_consistent_Key-Value_store/keyValueStore/lib/protobuf-java-3.4.0.jar"
 #port
-java -classpath bin/classes:$LIB_PATH Client $1
+java -classpath bin/classes:$LIB_PATH Client $1 $2
