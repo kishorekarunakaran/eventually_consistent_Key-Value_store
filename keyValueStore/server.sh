305907243352c33d3747f5a76e61339b98cdaa95
Surikm09 <skoneti1@binghamton.edu>
2017-12-04 17:56:01 -0500

    command line arguments argv[3]-{read repair or hinted handoff}

--- keyValueStore/server.sh
+++ keyValueStore/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash +vx
 LIB_PATH=$"/home/phao3/protobuf/protobuf-3.4.0/java/core/target/protobuf.jar"
 #port
-java -classpath bin/classes:$LIB_PATH keyValueStore.server.ReplicaServer $1 $2 $3
+java -classpath bin/classes:$LIB_PATH keyValueStore.server.ReplicaServer $1 $2 $3 $4
